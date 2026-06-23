e19e55fb3619972c985028a11303c08a9eb7ea4b
ATN-lang <anrompro@gmail.com>
2026-06-23 13:08:07 +0700

    Set base path to '/Cát Lái sô/' in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({mode}) => {
   }
 
   return {
+    base: '/Cát Lái sô/'
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
